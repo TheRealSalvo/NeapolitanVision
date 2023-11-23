773ae67de4c2b90aee6d4d9657f5007fac324af1
TheRealSalvo <salva0729@gmail.com>
2023-11-23 04:45:22 +0100

    added file

---
+++ AIVision/Camera/DetectableItemsListView.swift
@@ -0,0 +1,18 @@
+//
+//  DetectableItemsListView.swift
+//  AIVision
+//
+//  Created by Salvatore Attanasio on 23/11/23.
+//
+
+import SwiftUI
+
+struct DetectableItemsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetectableItemsListView()
+}
